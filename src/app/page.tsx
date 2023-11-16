0b168215b2c32138fbf2df1c9299d62edbaf4367
SkylerA <skylerarabak@gmail.com>
2023-11-16 09:25:16 -0800

    Isolate css to global.css since project is small

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// import styles from './page.module.css'
 import Search from './components/Search'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <Search></Search>
     </main>
   )
