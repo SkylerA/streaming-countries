973f86ab382509dee8c3dcf546bec26c96bedb4c
SkylerA <skylerarabak@gmail.com>
2023-11-17 09:19:25 -0800

    Update about

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -11,6 +11,7 @@ const About = (props: Props) => {
                     <li>This searches all countries on <a href="https://www.movieofthenight.com/" target="_blank">Movie of the Night</a> for free/subscription streaming options</li>
                     <ul>
                         <li>All other results (Rent/Buy) are removed</li>
+                        <li>API Data seems to have some inaccuracies so YMMV</li>
                     </ul>
                     <li>The goal is to simplify finding when you can use a VPN to stream from a service you already  have</li>
                     <li>Check <a href='https://www.justwatch.com' target='_blank'>JustWatch</a> first as it has more complete results including free w/ ads options</li>
